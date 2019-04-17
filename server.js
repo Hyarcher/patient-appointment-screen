defcdc1e1aa9db9597bd033ef6aa5d83208902af
Harry Chapman <HyarcherHyarcher@gmail.com>
2019-04-17 17:46:21 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+console.log("all set... please go to http://localhost:8080 in your chosen web browser");
+
+app.use(express.static('public'));
+
+app.get('/',function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/css/style.css',function(req, res){
+  res.sendFile(__dirname + '/css/style.css');
+});
