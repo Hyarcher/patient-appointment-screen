cfa676acc306b9a44ad3fbf0e747d6d17f36c611
Harry Chapman <HyarcherHyarcher@gmail.com>
2019-05-09 16:25:25 +0100

    Delete utils.js

--- public/utils.js
+++
@@ -1,8 +0,0 @@
-function getAllPatients(callback) {
-  let patientRequest = new XMLHttpRequest();
-  patientRequest.open('GET', '/allPatients');
-  patientRequest.onload = function() {
-    callback(JSON.parse(patientRequest.responseText));
-  }
-  patientRequest.send();
-}
