f0eae4f42adcd156e6c260a7bfb0649a78cd3b7a
Harry Chapman <HyarcherHyarcher@gmail.com>
2019-05-09 16:23:21 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const fs = require("fs");
 
 console.log("The server is now running! Please go to http://localhost:8080 in your chosen web browser");
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.listen(8080);
 
+app.get("/",function(req, res){
+  res.sendFile(__dirname + "/public/settings.html");
+});
 
 // Adds patient from file.
 // @param {string} fileLocation - The JSON file location.
@@ -30,7 +33,7 @@ function addToPatientFile(fileLocation, patientInfo) {
     })
 
     fs.writeFile(fileLocation, JSON.stringify(patients), function() {
-      console.log('File written.');
+      console.log("File written.");
     });
   });
 }
@@ -50,7 +53,7 @@ function deletePatientFromFile(fileLocation, patientName){
       patients = patients.filter((patient) => {return patient.patient != patientName});
 
       fs.writeFile(fileLocation, JSON.stringify(patients), function() {
-        console.log('successfully deleted patient from file.');
+        console.log("successfully deleted patient from file.");
       });
     });
 }
@@ -59,13 +62,13 @@ function deletePatientFromFile(fileLocation, patientName){
 // Routes
 //
 
-app.get('/allPatients', function(req, res) {
-  let patientInfo = fs.readFileSync('patients.json');
+app.get("/allPatients", function(req, res) {
+  let patientInfo = fs.readFileSync("patients.json");
   let patients = JSON.parse(patientInfo);
   res.status(200).send(patients);
 });
 
-app.post('/addPatient', function(req, res) {
+app.post("/addPatient", function(req, res) {
   let patientInfo = {
     patient: req.body.patient,
     doctor: req.body.doctor,
@@ -73,18 +76,18 @@ app.post('/addPatient', function(req, res) {
     time: req.body.time,
   };
 
-  console.log('Data: ');
+  console.log("Data: ");
   console.log(req.body);
 
-  addToPatientFile('patients.json', patientInfo);
+  addToPatientFile("patients.json", patientInfo);
   res.status(200).send(JSON.stringify(patientInfo));
 });
 
 
-app.post('/deletePatient', function(req, res) {
+app.post("/deletePatient", function(req, res) {
     let patientName = req.body.patient;
     if (patientName){
-        deletePatientFromFile('patients.json', patientName);
+        deletePatientFromFile("patients.json", patientName);
         res.status(200).send();
     } else{
         res.status(400).send();
